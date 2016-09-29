abe1a4bbb28ddba1c55ed9e18a723ab2821b50ad
Kevin J. Scott <kevinjscott@gmail.com>
2016-09-29 12:04:27 -0400

    try to allow running from file:///

--- src/scripts/app/mApp.js
+++ src/scripts/app/mApp.js
@@ -36,5 +36,9 @@ angular.module('mApp', [ngTouch, ngSanitize, uiRouter, mAnimations, mCtrls, mDir
 
         $urlRouterProvider.otherwise('/');
 
-        $locationProvider.html5Mode(true);
+        // $locationProvider.html5Mode(true);
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+          });
     });
